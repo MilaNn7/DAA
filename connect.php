d5ad8fd9a2e9cd64ab4af402bc5f77da84c6c4e3
MilaNn7 <134263914+MilaNn7@users.noreply.github.com>
2024-06-03 14:04:58 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,14 @@
- $localhost = 'localhost';
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "userdb";
+$password = "databaza";
+$dbname = "northwind";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
