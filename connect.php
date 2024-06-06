f8b39a056b6b351f69e75148fd75a94c465779cf
MilaNn7 <134263914+MilaNn7@users.noreply.github.com>
2024-06-06 08:43:43 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "userdb";
 $password = "databaza";
-$dbname = "norhtwindmysql";
+$dbname = "northwindmysql";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
