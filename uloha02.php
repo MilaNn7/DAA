f8f5ed78e052859123e2ccc1ed0977e78d1e5921
MilaNn7 <134263914+MilaNn7@users.noreply.github.com>
2024-06-09 08:31:09 +0200

    Update uloha02.php

--- uloha02.php
+++ uloha02.php
@@ -1,14 +1,12 @@
-
 <!DOCTYPE html>
 <html lang="sk">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Úloha 02</title>
-    
 </head>
+    
 <body>
-
 <?php
 require_once "connect.php";
 ?>
